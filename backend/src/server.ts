3d6c21f2a6020aef4c46ed31edc6a65f25af6948
Gabriel Lima <gabastos.lima@gmail.com>
2020-10-14 20:46:17 -0300

    mvc

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,10 +2,11 @@ import express from 'express';
 
 import './database/connection';
 
+import routes from './routes';
+
 const app = express();
 
 app.use(express.json());
-
-app.get('/');
+app.use(routes);
 
 app.listen(3333, () => console.log('Server is running!'));
