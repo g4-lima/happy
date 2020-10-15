dfdc8f283a70c3768fbb2732ab284ed816fe4cbe
Gabriel Lima <gabastos.lima@gmail.com>
2020-10-15 14:33:47 -0300

    marker

---
+++ web/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus, FiArrowRight } from 'react-icons/fi';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import Leaflet from 'leaflet';
+
+import 'leaflet/dist/leaflet.css';
+
+import mapMarkerImg from '../assets/map-marker.svg';
+
+import '../styles/pages/orphanages-map.css';
+
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarkerImg,
+
+  iconSize: [58, 60],
+  iconAnchor: [29, 60],
+  popupAnchor: [170, 12],
+});
+
+const OrphanagesMap: React.FC = () => {
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy" />
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>Vinhedo</strong>
+          <span>São Paulo</span>
+        </footer>
+      </aside>
+
+      <Map
+        center={[-23.0422819, -46.9708041]}
+        zoom={14}
+        style={{ width: '100%', height: '100%' }}
+      >
+        {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
+
+        <Marker icon={mapIcon} position={[-23.0422819, -46.9708041]}>
+          <Popup
+            closeButton={false}
+            minWidth={240}
+            maxWidth={240}
+            className="map-popup"
+          >
+            Orfanato
+            <Link to="/">
+              <FiArrowRight size={20} color="#fff" />
+            </Link>
+          </Popup>
+        </Marker>
+      </Map>
+
+      <Link to="/" className="create-orphanage">
+        <FiPlus size={32} color="#fff" />
+      </Link>
+    </div>
+  );
+};
+
+export default OrphanagesMap;
